511acd8ea381b923dc0ab020d6358dd3d37c893c
GautamMehta19 <mehtag810@gmail.com>
2024-04-20 12:16:36 +0530

    changes in contact page

--- src/app/Components/Contact/Contact.js
+++ src/app/Components/Contact/Contact.js
@@ -1,13 +1,13 @@
 const Footer = () => {
     return (
-        <footer id="contact" className="bg-gray-800 py-6 text-gray-300">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row justify-between">
-                <div className="mb-4 md:mb-0">
+        <footer id="contact" className="bg-gray-500 py-10 text-white">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row justify-between bg-gray-600 py-3 rounded-md">
+                <div className="mb-4 md:mb-0 ">
                     <h2 className="text-xl font-bold mb-2">Contact Us</h2>
                     <p>Email: mehtag810@gmail.com</p>
                     <p>Phone: +91 9340924917</p>
                 </div>
-                <div className="mb-4 md:mb-0">
+                <div className="mb-4 md:mb-0 ">
                     <h2 className="text-xl font-bold mb-2">Important Links</h2>
                     <ul>
                         <li>Privacy Policy</li>
@@ -16,7 +16,10 @@ const Footer = () => {
                     </ul>
                 </div>
             </div>
-            <div className="text-center">Made by Gautam Mehta</div>
+            <div className="text-center text-white px-4 py-3 rounded-md flex flex-col">
+                <h2 >© Copyright 2024</h2>
+                <h2 >Made by <span className="font-bold">Gautam Mehta</span></h2>
+            </div>
         </footer>
     );
 };
