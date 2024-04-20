ea5180d7a18ea978d5485a15be4c5fbf44fbda24
GautamMehta19 <mehtag810@gmail.com>
2024-04-20 10:47:35 +0530

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import HomePage from "../../Components/Home/Home";
-import Navbar from "../../Components/Navbar/Navbar";
-import AboutPage from "../../Components/About/About";
-import Skills from "../../Components/Skills/Skills";
-import Projects from "../../Components/Projects/Projects";
-import Contact from "../../Components/Contact/Contact";
+import HomePage from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import AboutPage from "./Components/About/About";
+import Skills from "./Components/Skills/Skills";
+import Projects from "./Components/Projects/Projects";
+import Contact from "./Components/Contact/Contact";
 
 export default function Home() {
   return (
